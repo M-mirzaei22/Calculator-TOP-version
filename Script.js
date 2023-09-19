d8e9b6150d3c731d7185e205bfb1168cdce1e393
M_mirzaei <M_mirzaei.NL@proton.me>
2023-09-19 19:00:28 +0330

    step 2 &3 added

--- Script.js
+++ Script.js
@@ -1,17 +1,20 @@
 // temp prompts for user input . this is only testing to see if the code works
 let a = prompt("Please enter a number: ");
 let b = prompt("Please enter a second number: ");
+let c = prompt("What operator would you like to use(+ , - , * , /) ???; ");
 let num1 = parseFloat(a);
 let num2 = parseFloat(b);
+let operator = c;
 console.log("num1 :" , num1);
 console.log("num2 :" , num2);
-//will delete this later 
+console.log("chosen operator :" , operator);
+//will delete this later -- this part is temperory until we get bottoms
 
 // calling  functionsto test the prompts values
-add(num1, num2);
-subtract(num1, num2);
-multiply(num1, num2);
-divide(num1, num2);
+// add(num1, num2);
+// subtract(num1, num2);
+// multiply(num1, num2);
+// divide(num1, num2);
 
 //now lets make the functions that we need 
 function add(num1, num2) {
@@ -33,7 +36,38 @@ function multiply(num1, num2) {
 };
 
 function divide(num1, num2) {
-    let divideValue = (num1 / num2)
-    console.log("divide :" , divideValue)
+    let divideValue = (num1 / num2);
+    //error check for division by 0
+    if (divideValue == "Infinity") {
+        return alert("this an ERROR!!! you know i can't divide by zero#_# YOU SHOULD KNOW BETTER ....");
+    }else return console.log("divide :" , divideValue);
     //return divideValue;
-}
+};
+
+//use operator variable to determine which function to call
+function operate(operator , num1 , num2 ) {
+    // research for what the fuck is switch
+    switch(operator) {
+        case"+":
+            console.log("passing through switch +");
+            add(num1 , num2);
+            break;
+        case"-":
+            console.log("passing through switch -");
+            subtract(num1 , num2);
+            break;
+        case"*":
+            console.log("passing through switch *");
+            multiply(num1 , num2);
+            break;
+        case"/":
+            console.log("passing through switch /");
+            divide(num1 , num2);
+            break;
+        default:
+            alert("ERROR !!! DIDN'T recive an operator , or type not as expected!!! ");
+    };
+};
+
+// now it's time to call the operate function and pass through the arguments that we got from prompts
+operate(operator , num1 , num2 );
